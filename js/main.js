8b3f4b096447db505c46a4fe0db2599b668919d8
xapenny <xapenny@icloud.com>
2019-06-22 13:47:03 +0800

    Updated index.html

--- js/main.js
+++ js/main.js
@@ -28,10 +28,7 @@
 	});
 
 	$("a[href*=\\#]").on("click", function (event) {
-		event.preventDefault();
 
-		$("html, body").animate({
-			scrollTop: $(this.hash).offset().top
 		}, 500);
 	});
 })();
